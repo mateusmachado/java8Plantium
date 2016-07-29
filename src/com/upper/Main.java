30ff8c79b24ac723d5fa41f27225b2d06f938b4a
mmachado.csp@gmail.com <mmachado.csp@gmail.com>
2016-07-29 10:36:21 -0300

    added flatMap test

--- src/com/upper/Main.java
+++ src/com/upper/Main.java
@@ -3,6 +3,7 @@ package com.upper;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -11,6 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 		upperCase(Arrays.asList("osx", "win", "linux"));
 		System.out.println(collectionLessThan4characters(Arrays.asList("osx", "win", "linux")));
+		System.out.println(flattenCollection(Arrays.asList("Viktor", "Farcic"), Arrays.asList("John", "Doe", "Third")));
 	}
 
 	public static List<String> upperCase(List<String> collection) {		
@@ -19,5 +21,9 @@ public class Main {
 	
 	public static List<String> collectionLessThan4characters(List<String> collection){
 		return collection.stream().filter(x -> x.length() < 4).collect(Collectors.toList());
+	}
+	
+	public static List<String> flattenCollection(List<String> collection1, List<String> collection2){
+		return Stream.of(collection1, collection2).flatMap(c1 -> c1.stream()).collect(Collectors.toList());
 	}
 }
---
+++ test/com/flatmap/FlatCollection.java
@@ -0,0 +1,19 @@
+package com.flatmap;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.upper.Main;
+
+public class FlatCollection {
+	
+	@Test
+    public void transformShouldFlattenCollection() {
+        List<String> expected = Arrays.asList("Viktor", "Farcic", "John", "Doe", "Third");
+        assertEquals(Main.flattenCollection(Arrays.asList("Viktor", "Farcic"), Arrays.asList("John", "Doe", "Third")), expected);
+    }
+}
